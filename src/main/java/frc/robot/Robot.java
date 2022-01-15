880aefe319c68146b636c95cefc419a6dbf25ebc
Jeffrey Morris <ffejcsirrom@gmail.com>
2022-01-15 10:22:01 -0600

    update robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,9 +1,5 @@
-
 package frc.robot;
 
-import com.revrobotics.REVPhysicsSim;
-
-import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -12,82 +8,69 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.kAutoTrajs;
 
-
 public class Robot extends TimedRobot {
-	public static Field2d kField;
-	public static kAutoTrajs kSelectedAutoTrajs;
-	private RobotContainer m_robotContainer;
-	private SendableChooser<kAutoTrajs> m_autoChooser = new SendableChooser<kAutoTrajs>();
+  public static Field2d m_field;
+  public static kAutoTrajs m_selectedAutoTrajs;
+  public static RobotContainer m_container;
+  private static SendableChooser<kAutoTrajs> m_autoChooser = new SendableChooser<kAutoTrajs>();
 
+  @Override
+  public void robotInit() {
+    m_container = new RobotContainer();
+    m_field = new Field2d();
+    SmartDashboard.putData("field", m_field);
 
-	@Override
-	public void robotInit() {
-		m_robotContainer = new RobotContainer();
-		kField = new Field2d();
-		SmartDashboard.putData("field", kField);
+    // auto chooser
+    // m_autoChooser.setDefaultOption("Default", kAutoTrajs.DEFAULT);
+    m_autoChooser.setDefaultOption("ball to hp then layup", kAutoTrajs.BALLTOHPTHENLAYUP);
+    SmartDashboard.putData(m_autoChooser);
+  }
 
-		// auto chooser
-		//m_autoChooser.setDefaultOption("Default", kAutoTrajs.DEFAULT);
-		m_autoChooser.setDefaultOption("ball to hp then layup", kAutoTrajs.BALLTOHPTHENLAYUP);
-		SmartDashboard.putData(m_autoChooser);
-	}
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
 
-	@Override
-	public void robotPeriodic() {
-		CommandScheduler.getInstance().run();
-	}
-	
-	@Override
-	public void autonomousInit() {
-		// Stops all previously running commands.
-		CommandScheduler.getInstance().cancelAll();
-		kSelectedAutoTrajs = m_autoChooser.getSelected();
-		SequentialCommandGroup autoCommand = m_robotContainer.getAutonomousCommand(m_autoChooser.getSelected());
+    // Ensure the controller(s) are always configured / connected
+    Gamepads.configure();
+  }
 
-		if (autoCommand != null) {
-			autoCommand.schedule();
-		}
-	}
+  @Override
+  public void autonomousInit() {
+    // Stops all previously running commands.
+    CommandScheduler.getInstance().cancelAll();
+    m_selectedAutoTrajs = m_autoChooser.getSelected();
+    SequentialCommandGroup autoCommand =
+        m_container.getAutonomousCommand(m_autoChooser.getSelected());
 
-	@Override
-	public void autonomousPeriodic() {
-	}
+    if (autoCommand != null) {
+      autoCommand.schedule();
+    }
+  }
 
-	@Override
-	public void teleopInit() {
-		// Stops all previously running commands.
-		CommandScheduler.getInstance().cancelAll();
-	}
+  @Override
+  public void autonomousPeriodic() {}
 
-	@Override
-	public void teleopPeriodic() {
-	}
+  @Override
+  public void teleopInit() {
+    // Stops all previously running commands.
+    CommandScheduler.getInstance().cancelAll();
+  }
 
-	@Override
-	public void simulationInit() {
-		REVPhysicsSim.getInstance().addSparkMax(m_robotContainer.m_drivetrain.m_left, DCMotor.getNEO(1));
-		REVPhysicsSim.getInstance().addSparkMax(m_robotContainer.m_drivetrain.m_right, DCMotor.getNEO(1));
-	}
+  @Override
+  public void teleopPeriodic() {}
 
-	@Override
-	public void simulationPeriodic() {
-		REVPhysicsSim.getInstance().run();
-	}
+  @Override
+  public void simulationInit() {}
 
-	/**
-	 * 
-	 * LEAVE DISABLED METHODS ALONE
-	 * only overridden to stop the default override me message.
-	 * 
-	 */
-	@Override
-	public void disabledInit() {
-		// DONT PUT STUFF HERE
-	}
+  @Override
+  public void simulationPeriodic() {}
 
-	@Override
-	public void disabledPeriodic() {
-		// DONT PUT STUFF HERE
-	}
+  @Override
+  public void disabledInit() {
+    CommandScheduler.getInstance().cancelAll();
+    Gamepads.resetConfig();
+  }
 
+  @Override
+  public void disabledPeriodic() {}
 }
