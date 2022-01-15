017efb0f258f630bc07e668ded2f51e1d538babd
Jeffrey Morris <ffejcsirrom@gmail.com>
2022-01-15 10:23:28 -0600

    nonworking auto

--- src/main/java/frc/robot/commands/auto/BallToHpThenLayup.java
+++ src/main/java/frc/robot/commands/auto/BallToHpThenLayup.java
@@ -5,33 +5,40 @@ import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.kAutoTrajs;
 import frc.robot.Robot;
-import frc.robot.subsystems.DiffDrivetrain;;
+import frc.robot.subsystems.DifferentialDrivetrain;
 
 public class BallToHpThenLayup extends SequentialCommandGroup {
 
-	kAutoTrajs kAuto = kAutoTrajs.BALLTOHPTHENLAYUP;
+  DifferentialDrivetrain m_drivetrain;
+  kAutoTrajs kAuto = kAutoTrajs.BALLTOHPTHENLAYUP;
 
-	public BallToHpThenLayup(DiffDrivetrain drive) {
+  public BallToHpThenLayup(DifferentialDrivetrain drivetrain) {
 
-		
-		drive.restartOdometry(kAuto.kTrajs[0].getInitialPose());
+    m_drivetrain = drivetrain;
+    addRequirements(m_drivetrain);
 
-		for (int i = 0; i < kAuto.kTrajs.length; i++) {
-			Robot.kField.getObject("Auto " + i + " : " + kAuto.kTrajs[i].toString()).setTrajectory(kAuto.kTrajs[i]);
+    for (int i = 0; i < kAuto.kTrajs.length; i++) {
+      Robot.m_field
+          .getObject("Auto " + i + " : " + ((Object) kAuto.kTrajs).toString())
+          .setTrajectory(kAuto.kTrajs[i]);
 
-			addCommands(new RamseteCommand(
-					kAuto.kTrajs[i],
-					drive::getPose,
-					new RamseteController(2.0, 0.7),
-					drive.getFeedForward(),
-					drive.getKinematics(),
-					drive::getSpeeds,
-					drive.getLeftPIDController(),
-					drive.getRightPIDController(),
-					drive::setOutput,
-					drive));
-		}
+      addCommands(
+          new RamseteCommand(
+              kAuto.kTrajs[i],
+              m_drivetrain::getPose,
+              new RamseteController(2.0, 0.7),
+              m_drivetrain.getFeedForward(),
+              m_drivetrain.getKinematics(),
+              m_drivetrain::getSpeeds,
+              m_drivetrain.getLeftPIDController(),
+              m_drivetrain.getRightPIDController(),
+              m_drivetrain::setOutput,
+              m_drivetrain));
+    }
+  }
 
-	}
-
+  @Override
+  public void initialize() {
+    m_drivetrain.restartOdometry(kAuto.kTrajs[0].getInitialPose());
+  }
 }
