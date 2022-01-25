60a584d67ce7aa4868b0e4bba404001371512a7c
Jeffrey Morris <ffejsirrom.seb@gmail.com>
2022-01-25 13:33:03 -0600

    update Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,32 +1,42 @@
 package frc.robot;
 
-import java.io.IOException;
-import java.nio.file.Path;
-
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryUtil;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
+import java.io.IOException;
+import java.nio.file.Path;
 
 /**
- * All units should be in SI.
+ * A class to contain all things that remain constant and should be easily changed, without breaking
+ * other code.
+ *
+ * <p>All measurements should be in SI units.</p>
+ *
+ * @author Jeffrey Morris | Tigerbotics 7125
  */
 public class Constants {
 
+  // Shuffleboard tab names
+  public static final String kPreGameTabName = "Pre-Game";
+  public static final String kAutoTabName = "Auto";
+  public static final String kTeleopTabName = "Teleop";
+
   // Differential Drivetrain
-  public static final int kDDLeftDeviceId = 1;
-  public static final int kDDRightDeviceId = 2;
+  public static final int kDDLeftDeviceId = 10;
+  public static final int kDDRightDeviceId = 20;
 
   public static final double kDDWheelBaseWidth = Units.inchesToMeters(23);
   public static final double kDDWheelDiameter = Units.inchesToMeters(6);
 
   public static final double kDDGearRatio = 10.71;
-  public static final double kDDRPMToMPSConversionFactor = 1.0 / (kDDGearRatio * Math.PI * kDDWheelDiameter) / 60.0;
-  public static final double kDDDistancePerPulse = 1.0 / (42 / kDDGearRatio * (Math.PI * kDDWheelDiameter));
+  public static final double kDDRPMToMPSConversionFactor =
+      1.0 / (kDDGearRatio * Math.PI * kDDWheelDiameter) / 60.0;
+  public static final double kDDDistancePerPulse =
+      1.0 / (42 / kDDGearRatio * (Math.PI * kDDWheelDiameter));
 
   // Mecanum Drivetrain
   public static final int kMDLeftFrontId = 1;
@@ -39,31 +49,31 @@ public class Constants {
   public static final Translation2d kMDRightFrontOffset = new Translation2d(1, -1); // TODO
   public static final Translation2d kMDRightRearOffset = new Translation2d(-1, -1); // TODO
 
-
   public static final MotorType kMDMotorType = MotorType.kBrushless;
 
   public static final double kMDWheelBaseWidth = Units.inchesToMeters(23);
   public static final double kMDWheelDiameter = Units.inchesToMeters(6);
 
   public static final double kMDGearRatio = 10.71;
-  public static final double kMDRPMtoMPSConversionFactor = 1.0 / (kMDGearRatio * (Math.PI * kMDWheelDiameter));
-  public static final double kMDDistancePerPulse = 1.0
-      / (42 /* Hall Effects CPR */ / kMDGearRatio * (Math.PI * kMDWheelDiameter));
+  public static final double kMDRPMtoMPSConversionFactor =
+      1.0 / (kMDGearRatio * (Math.PI * kMDWheelDiameter));
+  public static final double kMDDistancePerPulse =
+      1.0 / (42 /* Hall Effects CPR */ / kMDGearRatio * (Math.PI * kMDWheelDiameter));
 
   // Misc
-  public static final int kPDPId = 0;
+  public static final int kPowerDistributionPanelId = 0;
   public static final int kPigeonId = 50;
 
   // Auto Trajectories & File Paths
   public enum kAutoTrajs {
     DEFAULT(
         new Path[] {
-            Filesystem.getDeployDirectory().toPath().resolve("paths/exitTarmac.wpilib.json")
+          Filesystem.getDeployDirectory().toPath().resolve("paths/exitTarmac.wpilib.json")
         }),
     BALLTOHPTHENLAYUP(
         new Path[] {
-            Filesystem.getDeployDirectory().toPath().resolve("paths/BallToHP.wpilib.json"),
-            Filesystem.getDeployDirectory().toPath().resolve("paths/terminalToHub.wpilib.json")
+          Filesystem.getDeployDirectory().toPath().resolve("paths/BallToHP.wpilib.json"),
+          Filesystem.getDeployDirectory().toPath().resolve("paths/terminalToHub.wpilib.json")
         });
 
     public Trajectory[] kTrajs;
