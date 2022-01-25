bf97ea4246f70b6ea8b3389d8b10929de75fb503
Jeffrey Morris <ffejsirrom.seb@gmail.com>
2022-01-25 13:32:20 -0600

    create AutonomousCommand class

---
+++ src/main/java/frc/lib/util/AutonomousCommand.java
@@ -0,0 +1,13 @@
+package frc.lib.util;
+
+import edu.wpi.first.wpilibj2.command.CommandGroupBase;
+
+/**
+ * An class for autonomous commands with useful functions.
+ *
+ * @author Jeffrey Morris | Tigerbotics 7125
+ */
+public abstract class AutonomousCommand extends CommandGroupBase {
+
+  public abstract void preview();
+}
