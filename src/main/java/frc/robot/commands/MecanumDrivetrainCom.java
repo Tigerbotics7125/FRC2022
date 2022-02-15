285ca2a840df1892e81a4db91c10e4e1adb64265
Jeffrey Morris <ffejsirrom.seb@gmail.com>
2022-02-14 21:45:56 -0600

    create MecDriveCom

---
+++ src/main/java/frc/robot/commands/MecanumDrivetrainCom.java
@@ -0,0 +1,38 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Gamepads;
+import frc.robot.subsystems.MecanumDrivetrainSub;
+
+/**
+ * Drives a Mecanum
+ *
+ * @author Jeffrey Morris | Tigerbotics 7125
+ */
+public class MecanumDrivetrainCom extends CommandBase {
+
+  MecanumDrivetrainSub m_drivetrain;
+
+  public MecanumDrivetrainCom(MecanumDrivetrainSub drivetrain) {
+    addRequirements(drivetrain);
+    m_drivetrain = drivetrain;
+  }
+
+  @Override
+  public void initialize() {}
+
+  @Override
+  public void execute() {
+    m_drivetrain.drive(
+        Gamepads.getMecaDriveXJoystick().getX(),
+        Gamepads.getMecaDriveYJoystick().getY(),
+        Gamepads.getMecaDriveZJoystick().getZ(),
+        true);
+  }
+
+  // Keep command always active.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
