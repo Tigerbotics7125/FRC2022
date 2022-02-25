00afea4e0b238c55636ff32547106ffa67e323db
Jeffrey Morris <ffejcsirrom@gmail.com>
2022-02-24 21:56:44 -0600

    create AutnomousCommand interface

---
+++ src/main/java/frc/lib/command/AutonomousCommand.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (C) 2022, Tigerbotics' team members and all other contributors.
+ * Open source software; you can modify and/or share this software.
+ */
+package frc.lib.command;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public interface AutonomousCommand extends Command {
+
+    public Pose2d getInitialPose();
+
+    public void preview();
+}
