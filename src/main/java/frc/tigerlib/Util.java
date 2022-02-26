344daa3f69557ae973c71b59dad728e7ac281b00
Jeffrey Morris <ffejsirrom.seb@gmail.com>
2022-02-26 09:22:02 -0600

    update Util

--- src/main/java/frc/tigerlib/Util.java
+++ src/main/java/frc/tigerlib/Util.java
@@ -7,68 +7,96 @@ package frc.tigerlib;
 public class Util {
 
     /**
-     * combines {@link #ramp}, {@link #deadband}, and {@link #clamp} into one such that [min,
-     * -deadband][deadband, max] and joystick is exponential, preserving sign.
-     *
-     * @param joystick
-     * @param exp
-     * @param deadband
-     * @param min
-     * @param max
-     * @return
-     */
-    public static double scaledDeadbandClamp(
-            double joystick, int exp, double deadband, double min, double max) {
-        joystick = ramp(joystick, exp);
-        joystick = deadband(joystick, deadband);
-        joystick = clamp(joystick, min, max);
-        return joystick;
-    }
-
-    /**
      * Multiplies a value by an exponent, preserving sign.
      *
-     * @param joystick
-     * @param exp
+     * @param x input
+     * @param exp exponent
      * @return
      */
-    public static double ramp(double joystick, int exp) {
-        if (joystick < 0) {
-            return -Math.pow(-joystick, exp);
+    public static double ramp(double x, double exp) {
+        if (x < 0) {
+            return -Math.pow(-x, exp);
         } else {
-            return Math.pow(joystick, exp);
+            return Math.pow(x, exp);
         }
     }
 
     /**
      * Applies a deadband such that [-Double.MIN_VALUE, -deadband][deadband, Double.MAX_VALUE]
      *
-     * @param joystick
-     * @param deadband
+     * @param x input
+     * @param d deadband
      * @return
      */
-    public static double deadband(double joystick, double deadband) {
-        if (Math.abs(joystick) < deadband) {
+    public static double deadband(double x, double d) {
+        if (Math.abs(x) < d) {
             return 0;
         } else {
-            return joystick;
+            return x;
         }
     }
 
     /**
      * Clamps a value to a range such that [min, max]
      *
-     * @param joystick
+     * @param x input
      * @param min
      * @param max
      */
-    public static double clamp(double joystick, double min, double max) {
-        if (joystick < min) {
+    public static double clamp(double x, double min, double max) {
+        if (x < min) {
             return min;
-        } else if (joystick > max) {
+        } else if (x > max) {
             return max;
         } else {
-            return joystick;
+            return x;
         }
+    }
+
+    /**
+     * combines {@link #ramp}, {@link #deadband}, and {@link #clamp} into one such that [min,
+     * -deadband][deadband, max] and joystick is exponential, preserving sign.
+     *
+     * @param x input
+     * @param d deadband
+     * @param exp exponent
+     * @param min
+     * @param max
+     * @return
+     */
+    public static double scaledDeadbandClamp(
+            double x, double d, double exp, double min, double max) {
+        x = deadband(x, d);
+        x = ramp(x, exp);
+        x = clamp(x, min, max);
+        return x;
+    }
+
+    /**
+     * Applies {@link deadband} to the input, then applies sensitivity as a power onto the input.
+     * Graph available here: https://www.desmos.com/calculator/o91q83dudn
+     *
+     * @param x input
+     * @param d deadband
+     * @param s sensitivity
+     * @return where x < -deadband, -(x^s), where -deadband <= x <= deadband, 0, and where deadband
+     *     < x, x^s
+     */
+    public static double deadbandSensitivy(double x, double d, double s) {
+
+        x = deadband(x, d);
+
+        if (x < 0) {
+            return -Math.pow(x, s);
+        } else {
+            return Math.pow(x, s);
+        }
+    }
+
+    /** Applies {@link deadbandSensitivity} to the input, then clamps to nominal joystick range. */
+    public static double joystickDeadbandSensitivity(double joystick, double d, double s) {
+        joystick = deadbandSensitivy(joystick, d, s);
+        joystick = clamp(joystick, -1, 1);
+        return joystick;
     }
 }
