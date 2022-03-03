b5c0b34f303b94b5baf696111de88154c1279d69
Jeffrey Morris <ffejsirrom.seb@gmail.com>
2022-03-03 12:33:41 -0600

    update raiseejectlower command

--- src/main/java/frc/robot/subsystems/ArmSub.java
+++ src/main/java/frc/robot/subsystems/ArmSub.java
@@ -9,6 +9,7 @@ import static frc.robot.constants.ArmConstants.kId;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
@@ -18,6 +19,11 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.RobotContainer;
 
+/**
+ * Controls the arm of the robot.
+ * 
+ * @author Jeffrey Morris | Tigerbotics 7125
+ */
 public class ArmSub implements Subsystem {
 
     static final WPI_TalonSRX m_arm = new WPI_TalonSRX(kId);
@@ -46,17 +52,29 @@ public class ArmSub implements Subsystem {
 
     public Command getRaiseEjectLowerCommand() {
         return new SequentialCommandGroup(
-                new InstantCommand(() -> setUp()),
-                new WaitCommand(1.25),
+                // raise arm and scoot back
+                new ParallelRaceGroup(
+                    new RunCommand(() -> setUp()),
+                    new RunCommand(() -> RobotContainer.kDrivetrain.drive(0, -.75, 0)),
+                    new WaitCommand(1.5)
+                ),
+                // drive up to goal
                 new ParallelRaceGroup(
                         new RunCommand(() -> RobotContainer.kDrivetrain.drive(0, .75, 0)),
-                        new WaitCommand(1)),
-                new InstantCommand(() -> RobotContainer.kIntake.eject()),
-                new WaitCommand(.5),
-                new InstantCommand(() -> RobotContainer.kIntake.disable()),
+                        new WaitCommand(2)
+                ),
+                // eject ball
+                new SequentialCommandGroup(
+                    new InstantCommand(() -> RobotContainer.kIntake.eject()),
+                    new WaitCommand(.5),
+                    new InstantCommand(() -> RobotContainer.kIntake.disable())
+                ),
+                // lower arm and scoot back
                 new ParallelRaceGroup(
                         new RunCommand(() -> RobotContainer.kDrivetrain.drive(0, -.75, 0)),
-                        new WaitCommand(1)),
-                new InstantCommand(() -> setDown()));
+                        new WaitCommand(1.5),
+                        new InstantCommand(() -> setDown())
+                )
+                );
     }
 }
