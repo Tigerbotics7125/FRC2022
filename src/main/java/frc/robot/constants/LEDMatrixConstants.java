daa811fb8669541ea54fabee602e558f743481fd
Jeffrey Morris <ffejsirrom.seb@gmail.com>
2022-03-08 16:12:28 -0600

    led subsystem

---
+++ src/main/java/frc/robot/constants/LEDMatrixConstants.java
@@ -0,0 +1,11 @@
+package frc.robot.constants;
+
+import frc.tigerlib.ledmatrix.Gif;
+
+public class LEDMatrixConstants {
+    public static final int kWidth = 15;
+    public static final int kHeight = 15;
+    public static final boolean kSerpentine = true;
+    public static final int kPWMPort = 0;
+    public static final Gif[] kGifs = new Gif[] {};
+}
